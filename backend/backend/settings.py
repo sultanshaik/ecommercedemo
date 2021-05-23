dab92080e4db7675c70725027b3ca55ae7426336
sultanshaik <sultanshaik.dev@gmail.com>
2021-05-22 20:17:31 -0700

    Add serialization for states

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Default primary key field type
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -11,13 +17,12 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer  = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for prod in products:
-        if prod['_id'] == pk:
-             product = prod
-             break
-    return Response(product);            
+    product = Product.objects.get(_id=pk)
+    serializer  = ProductSerializer(product, many=False)
+    return Response(serializer.data);            
